892163152e9c1abf9601ee42209a5121de38cbd9
backman-git <backman@nthu.edu.tw>
2023-07-25 22:13:33 +0800

    add Get command

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"net"
+	"strconv"
 )
 
 const (
 	PING = "PING"
 	SET  = "SET"
+	GET  = "GET"
+)
+
+// can optimize to
+const (
+	SIMPLE_STRING byte = '+'
+	ERRORS        byte = '-'
+	INTEGER       byte = ':'
+	BULK_STRING   byte = '$'
+	ARRAY         byte = '*'
 )
 
 type RClient struct {
 	net.Conn
+	rx chan string
 }
 
 func NewClient(serverAddr string) *RClient {
@@ -19,22 +33,38 @@ func NewClient(serverAddr string) *RClient {
 	if err != nil {
 		return nil
 	}
-	return &RClient{conn}
+	rClient := &RClient{conn, make(chan string)}
+	go func() {
+		rClient.readTx()
+	}()
+	return rClient
 }
 
-func (c RClient) Ping() string {
+func (c RClient) Ping() (string, error) {
 
 	c.sendCmd(PING)
-	return c.readResult()
+	res, err := c.readResponse()
+
+	return res.(string), err
 }
 
 func (c RClient) Set(key, val string) bool {
 	cmd := fmt.Sprintf("%s %s %s", SET, key, val)
 	c.sendCmd(cmd)
-	res := c.readResult()
+	res, _ := c.readResponse()
 
-	fmt.Println(res)
-	return true
+	if res.(string) == "OK" {
+		return true
+	}
+	return false
+}
+
+func (c RClient) Get(key string) string {
+	cmd := fmt.Sprintf("%s %s", GET, key)
+	c.sendCmd(cmd)
+	res, _ := c.readResponse()
+
+	return res.(string)
 }
 
 const CRLF = "\r\n"
@@ -44,23 +74,62 @@ func (c RClient) sendCmd(cmd string) {
 	return
 }
 
-func (c RClient) readResult() string {
-	res := make([]byte, 100)
-	_, err := c.Read(res)
-	if err != nil {
-		fmt.Println(err)
-		return ""
+func (c RClient) readTx() {
+	sc := bufio.NewScanner(c)
+	sc.Split(ScanCRLF)
+	for sc.Scan() {
+		c.rx <- sc.Text()
 	}
-	return string(res)
+}
+
+func (c RClient) readResponse() (interface{}, error) {
+	token := <-c.rx
+	//type
+	switch token[0] {
+	case SIMPLE_STRING, ERRORS:
+		return token[1:], nil
+	case INTEGER:
+		val, err := strconv.Atoi(token[1:])
+		if err != nil {
+			return 0, err
+		}
+		return val, nil
+	case ARRAY:
+		// TODO
+	case BULK_STRING:
+		str := <-c.rx
+		return str, nil
+	}
+	return nil, nil
 }
 
 func main() {
 
 	rc := NewClient("127.0.0.1:6379")
-
-	rc.Set("hi", "world")
+	fmt.Println(rc.Set("hi", "world"))
+	fmt.Println(rc.Get("hi"))
 }
 
-func scanToken() {
+// dropCR drops a terminal \r from the data.
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
+}
 
+func ScanCRLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.Index(data, []byte{'\r', '\n'}); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 2, dropCR(data[0:i]), nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), dropCR(data), nil
+	}
+	// Request more data.
+	return 0, nil, nil
 }
